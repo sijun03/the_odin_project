f2cb337a2a7d82f4507d8933207f07f8b832d8bb
Sijun <sijun03@gmail.com>
2021-02-22 21:05:41 +0300

    Learning java basics 22 Feb 2021

--- js_basics/cals.js
+++ js_basics/cals.js
@@ -1,1 +1,12 @@
-document.write(100);
+// javascript goes here!
+"use strict";
+let a = 101
+document.write(a);
+console.log("Hello world!");
+let username = "Tom";
+let adminname
+adminname = username;
+alert(adminname);
+/*
+alert(`Hey this is ${name}`)
+*/
