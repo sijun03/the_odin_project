1f2ec75867455751f7212d128511fe69e71aa59a
Sijun <sijun03@gmail.com>
2021-02-26 22:43:15 +0300

    Javascript output 26Feb2021

--- js_basics/cals.js
+++ js_basics/cals.js
@@ -1,12 +1,27 @@
 // javascript goes here!
 "use strict";
-let a = 101
-document.write(a);
-console.log("Hello world!");
-let username = "Tom";
-let adminname
-adminname = username;
-alert(adminname);
-/*
-alert(`Hey this is ${name}`)
-*/
+let a = "Not working";
+
+function alert_output()
+{
+    //output using popup
+    a = document.getElementById("txtinput").value;
+    alert(a);
+}
+
+function write_output()
+{   
+    // output using document write method
+    a = document.getElementById("txtinput").value;
+    document.getElementById("outputline").innerHTML = a;
+    /*document.write(a);*/
+}
+
+function console_output()
+{   //output using console
+    a = document.getElementById("txtinput").value;
+    console.log(a);
+}
+
+
+
