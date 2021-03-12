eed25a9cce6804dde08ffc11f2efee31c7d09a89
Sijun <sijun03@gmail.com>
2021-03-12 22:28:30 +0300

    Cals day 2 problem with addition, its getting concat

--- js_basics/cals.js
+++ js_basics/cals.js
@@ -1,27 +1,60 @@
 // javascript goes here!
 "use strict";
-let a = "Not working";
+let popthis = "Not working";
 
 function alert_output()
 {
     //output using popup
-    a = document.getElementById("txtinput").value;
-    alert(a);
+    popthis = document.getElementById("txtinput").value;
+    alert(popthis);
 }
 
 function write_output()
 {   
     // output using document write method
-    a = document.getElementById("txtinput").value;
-    document.getElementById("outputline").innerHTML = a;
+    popthis = document.getElementById("txtinput").value;
+    document.getElementById("outputline").innerHTML = popthis;
     /*document.write(a);*/
 }
 
 function console_output()
 {   //output using console
-    a = document.getElementById("txtinput").value;
-    console.log(a);
+    popthis = document.getElementById("txtinput").value;
+    console.log(popthis);
 }
 
+function add_numbers()
+{   //calc add
+    let a = 0, b = 0, r = 0;
+    a = document.getElementById("n1").value;
+    b = document.getElementById("n2").value;
+    r = a + b;
+    alert(r);
+}
 
+function substract_numbers()
+{   //calc minus
+    let a = 0, b = 0, r = 0;
+    a = document.getElementById("n1").value;
+    b = document.getElementById("n2").value;
+    r = a - b;
+    alert(r);
+}
 
+function multiply_numbers()
+{   //calc multiply
+    let a = 0, b = 0, r = 0;
+    a = document.getElementById("n1").value;
+    b = document.getElementById("n2").value;
+    r = a * b;
+    alert(r);
+}
+
+function div_numbers()
+{   //calc divide
+    let a = 0, b = 0, r = 0;
+    a = document.getElementById("n1").value;
+    b = document.getElementById("n2").value;
+    r = a / b;
+    alert(r);
+}
